fa28af0d92dfbecfe928ad40cf84e421b70b2ed2
ThameshP <72529324+ThameshP@users.noreply.github.com>
2022-11-11 14:58:47 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.sendFile("/index.html",{root: __dirname})
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile("/login.html",{root: __dirname})
+})
+
+app.get('/signup', (req, res) => {
+  res.sendFile("/signup.html",{root: __dirname})
+})
+
+app.post('/getnotes', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile("/signup.html", {root: __dirname})
+})
+
+app.post('/login', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile("/signup.html", {root: __dirname})
+})
+
+app.post('/signup', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile("/signup.html", {root: __dirname})
+})
+
+app.post('/addnotes', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile("/signup.html", {root: __dirname})
+})
+
+app.post('/deletenotes', (req, res) => {
+  const {userToken } = req.body
+  res.sendFile("/signup.html", {root: __dirname})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost: ${port}`)
+})
---
+++ notes.js
@@ -0,0 +1,34 @@
+
+// Example POST method implementation:
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+//fetch Notes
+const user = JSON.parse(localStorage.getItem('user'))
+// if(!user.token || !user){
+//   window.location= "/login"
+// }
+const notes = postData('/getnotes', {token: user.token})
+
+//This will populate the notes
+notes.then((notes)=>{
+  console.log("Notes")
+})
+
+//Add a click listener for submit
+let submit = document.getElementbyID("submit")
+submit.addEventListener("click", ()=> {
+  let title = document.getElementbyID("title")
+  let desc = document.getElementbyID("desc")
+  console.log("Submitting this data", title, desc)
+})
